428ef0d697e909835b1e9da84d7795cc3ddcf2ed
KolyaFaluk <kolya_faluk@mail.ru>
2016-12-19 19:32:01 +0300

    Add files via upload

---
+++ Controllers/FilmsController.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Cinema.Models;
+
+namespace Cinema.Controllers
+{
+    public class FilmsController : Controller
+    {
+        private CinemaContext db = new CinemaContext();
+
+
+        // GET: Films
+        public ActionResult Index(int? genre)
+        {
+            IQueryable<Film> films = db.Films.Include(p => p.Genre);
+            if (genre != null && genre != 0)
+            {
+                films = films.Where(a => a.GenreId == genre);
+            }
+
+            List<Genre> genres = db.Genres.ToList();
+
+            genres.Insert(0, new Genre { Name = "Все", Id = 0 });
+
+            FilmsListViewModel plvm = new FilmsListViewModel
+            {
+                Films = films.ToList(),
+                Genres = new SelectList(genres, "Id", "Name")
+            };
+            return View(plvm);
+        }
+
+        // GET: Films/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Film film = db.Films.Find(id);
+            if (film == null)
+            {
+                return HttpNotFound();
+            }
+            return View(film);
+        }
+
+
+        
+        public ActionResult Seance(int? id)
+        {
+            ViewBag.Iddd = id;
+            List<Seance> seances = db.Seances.Include(a => a.Film).ToList();
+            seances = db.Seances.Where(a => a.FilmId == id).ToList();
+
+            return View(seances);
+        }
+
+
+        // GET: Films/Create
+        public ActionResult CreateSeance()
+        {
+            return View();
+        }
+
+        // POST: Films/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult CreateSeance(Seance seance)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Seances.Add(seance);
+                db.SaveChanges();
+                return RedirectToAction("Seance");
+            }
+
+            return View(seance);
+        }
+
+
+        // GET: Films/Create
+        public ActionResult Create()
+        {
+            ViewBag.GenreId = new SelectList(db.Genres, "Id", "Name");
+            return View();
+        }
+
+        // POST: Films/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Name,Production,Year,Duration,GenreId")] Film film)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Films.Add(film);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.GenreId = new SelectList(db.Genres, "Id", "Name", film.GenreId);
+            return View(film);
+        }
+
+        // GET: Films/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Film film = db.Films.Find(id);
+            if (film == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.GenreId = new SelectList(db.Genres, "Id", "Name", film.GenreId);
+            return View(film);
+        }
+
+        // POST: Films/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Name,Production,Year,Duration,GenreId")] Film film)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(film).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.GenreId = new SelectList(db.Genres, "Id", "Name", film.GenreId);
+            return View(film);
+        }
+
+        // GET: Films/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Film film = db.Films.Find(id);
+            if (film == null)
+            {
+                return HttpNotFound();
+            }
+            return View(film);
+        }
+
+        // POST: Films/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Film film = db.Films.Find(id);
+            db.Films.Remove(film);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ Controllers/SeancesController.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Cinema.Models;
+
+namespace Cinema.Controllers
+{
+    public class SeancesController : Controller
+    {
+        private CinemaContext db = new CinemaContext();
+
+        // GET: Seances
+        public ActionResult Index()
+        {
+            var seances = db.Seances.Include(s => s.Film);
+            return View(seances.ToList());
+        }
+
+        // GET: Seances/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Seance seance = db.Seances.Find(id);
+            if (seance == null)
+            {
+                return HttpNotFound();
+            }
+            return View(seance);
+        }
+
+        // GET: Seances/Create
+        public ActionResult Create()
+        {
+            ViewBag.FilmId = new SelectList(db.Films, "Id", "Name");
+            return View();
+        }
+
+        // POST: Seances/Create
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,DateTime,PriceTicket,CountTickets,HallId,FilmId")] Seance seance)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Seances.Add(seance);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.FilmId = new SelectList(db.Films, "Id", "Name", seance.FilmId);
+            return View(seance);
+        }
+
+        // GET: Seances/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Seance seance = db.Seances.Find(id);
+            if (seance == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.FilmId = new SelectList(db.Films, "Id", "Name", seance.FilmId);
+            return View(seance);
+        }
+
+        // POST: Seances/Edit/5
+        // Чтобы защититься от атак чрезмерной передачи данных, включите определенные свойства, для которых следует установить привязку. Дополнительные 
+        // сведения см. в статье http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,DateTime,PriceTicket,CountTickets,HallId,FilmId")] Seance seance)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(seance).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.FilmId = new SelectList(db.Films, "Id", "Name", seance.FilmId);
+            return View(seance);
+        }
+
+        // GET: Seances/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Seance seance = db.Seances.Find(id);
+            Film film = db.Films.Find(id);
+            if (seance == null)
+            {
+                return HttpNotFound();
+            }
+            return View(seance);
+        }
+
+        // POST: Seances/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            
+            Seance seance = db.Seances.Find(id);
+            db.Seances.Remove(seance);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+        [HttpGet]
+        public ActionResult Ticket(int id)
+        {
+
+            var filmnames = db.Films.Where(p => p.Id == id);
+            List<Film> filmsn = filmnames.ToList();
+            ViewBag.FilmName = filmsn[0].Name;
+
+            var datetime = db.Seances.Where(c => c.Id == id);
+            List<Seance> vremya = datetime.ToList();
+            List<Seance> vremya1 = datetime.ToList();
+
+            ViewBag.DateTime = vremya[0].DateTime.ToShortDateString();
+            ViewBag.DateTime1 = vremya1[0].DateTime.ToShortTimeString();
+
+            var priceticket = db.Seances.Where(d => d.Id == id);
+            List<Seance> priceticke = priceticket.ToList();
+            ViewBag.PriceTicket = priceticke[0].PriceTicket;
+
+            var count = db.Seances.Where(e => e.Id == id);
+            List<Seance> countticket = count.ToList();
+            ViewBag.CountTickets = countticket[0].CountTickets;
+            // Ticket ticket = db.Tickets.Find(id);
+
+            var hall = db.Seances.Where(k => k.Id == id);
+            List<Seance> hallid = hall.ToList();
+            ViewBag.HallId = hallid[0].HallId;
+
+            return View();
+        }
+
+        
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        //public ActionResult Order(int id, Film film,  Seance seance, Order order, int count)
+      public ActionResult Ticket(Seance seance)
+        {
+            Order order = new Order();
+
+            var Seance = db.Seances.FirstOrDefault(c => c.Id == seance.Id);
+
+            var filmname = db.Films.FirstOrDefault(c => c.Id == Seance.FilmId).Name;
+            var price = Seance.PriceTicket;
+            var count = Seance.CountTickets;
+            var date = Seance.DateTime;
+            var orderCount = seance.CountTickets;
+
+            order.SeanceId = seance.Id;
+            order.FilmName = filmname;
+            order.PriceId = price;
+            order.CountId = orderCount;
+            order.TimeDate = date;
+
+            db.Orders.Add(order);
+            db.SaveChanges();
+            return View("Order",db.Orders);
+        }
+        
+
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
